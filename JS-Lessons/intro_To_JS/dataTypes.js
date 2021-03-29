26819959311bab7f571d4b12906cca26f081318a
chris <chrisbit.carrington@gmail.com>
2021-03-29 16:49:32 +0200

    JavaScript Projects

---
+++ JS-Lessons/intro_To_JS/dataTypes.js
@@ -0,0 +1,29 @@
+/*Data types are the classifications we give to the different kinds of data that we use in programming.
+In JavaScript, there are seven fundamental data types: */
+
+// Numbers :
+let number = 4;
+let decimal = 12.17;
+console.log('Number Types: ' + number + decimal);
+
+// Strings:
+let name = 'Chris';
+let lastName = 'Bonnin';
+console.log('String Types: ' + name + lastName);
+
+//Booleans:
+let truthy = true;
+let falsy = false;
+console.log('Booleans : ' + truthy + falsy);
+
+// Null represents intentional absence of value:
+let nothing = null;
+console.log('Null Type: ' + nothing);
+// Undefined: It also represents the absence of a value though its different from 'null'
+let absent = undefined;
+console.log('Undefined Type: ' + absent);
+
+// Object: Collections of related data:
+let data = <object data="Homes" type="String"></object>
+console.log('Object Type: ' + data);
+
---
+++ JS-Projects/GuessingGameJS/GuessingGame.js
@@ -0,0 +1,25 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('number');
+
+let guess = Math.floor(Math.random()* 20) + 1;
+let tries = 0
+btn.addEventListener('click',function (){
+
+    let answer = document.getElementById('userInput').value;
+    tries++
+    if(answer === guess){
+        output.innerHTML = `You got it !!!, the right number was ${guess}. it only took you ${tries} tries`
+    } else if(answer > guess){
+        output.innerHTML = "Too high, try again"
+    } else if(answer < guess){
+        output.innerHTML = 'Too low, try again'
+    }
+
+});
+
+/* function resetAll() {
+    document.getElementById('container').reset();
+} */
+
+
+
