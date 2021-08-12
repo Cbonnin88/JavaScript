a4d3b4e256c3db2e8fee54edc2f68b13bb130c76
Chris <chrisbit.carrington@gmail.com>
2021-08-12 12:41:19 +0200

    updated variable lesson

--- JS-Lessons/B_variables/variables_part2.js
+++ JS-Lessons/B_variables/variables_part2.js
@@ -14,9 +14,24 @@ let age = 32;
 let dob = "October 18th 1988"
 let rating = 7.8
 let isHuman = true
+console.log("let keyword:")
 console.log("His name is " + name + " and his is " + age + " years old as of" +
     " this year. He was born on " + dob + ". Chris gives this hotel a " + rating + " because of some minor" +
     " detail. Is Chris a human ?, the answer is " + isHuman)
+console.log("")
 
+// using const
+let pricePs5 = 500;
+const tvaTax = .20;
+const finalPrice = pricePs5 * (1 + tvaTax);
+console.log("const keyword:")
+console.log("A Playstation 5 in France will cost: " + finalPrice)
+
+
+/* Common pratices:
+Variable shouldn't begin with a number
+camelCase is the standard
+try to give your variables a clear name
+ */
 
 
