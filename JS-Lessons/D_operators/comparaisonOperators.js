bf7798d73ecc488fe33dd3d550f41ab5bca93573
Chris <chrisbit.carrington@gmail.com>
2021-08-13 17:42:40 +0200

    Added comparison operator lesson

---
+++ JS-Lessons/D_operators/comparaisonOperators.js
@@ -0,0 +1,27 @@
+/*
+Comparison operators are used in logical statements to determine equality or
+ difference between variables or values
+*/
+
+// The 'equals to' operator (===):
+let x = 8;
+let y = 8;
+console.log(x === y);
+/*
+Like other languages, there is also the == equals operator but in JavaScript it is
+ used less
+ */
+
+// The 'not equal' operator (!==):
+let a = 'house';
+let b = 'house';
+console.log(a !== b)
+
+// The 'greater than' operator (>):
+let up = 100;
+let down = 12;
+console.log(up > down);
+let c = 190;
+let d = 122;
+console.log(d > c);
+
