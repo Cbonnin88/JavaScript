6714272a5bdf594cad8a5ee4fe2626a31ac0bd0a
Chris <chrisbit.carrington@gmail.com>
2021-08-13 17:12:32 +0200

    Added operators lesson

---
+++ JS-Lessons/D_operators/operators.js
@@ -0,0 +1,36 @@
+/*
+Operators are used to assign and compare values, performing arithmetic operations and
+ more, they allow programmers to create a single value from one or more values
+ JavaScript as both Binary and Unary operators
+ */
+
+// Addition Operator:
+let num1 = 243;
+let num2 = 100;
+let add = num1 + num2;
+console.log('Addition Operator: 243 + 100 = ' + add);
+
+// Multiplication Operator:
+let num3 = 15;
+let num4 = 9;
+let multiply = num3 * num4;
+console.log('Multiplication Operator: 15 * 9 = ' + multiply);
+
+// Divison Operators:
+let num5 = 21.43;
+let num6 = 4.22;
+let divide = num5 / num6;
+console.log('Divison Operator: 21.43 / 4.22 = ' + divide);
+
+// Subtraction Operator:
+let num7 = 2021;
+let num8 = 1988;
+let subtract = num7 - num8;
+console.log('Subtraction Operator: 2021 - 1988 = ' + subtract);
+
+// Modulus Operator:
+let num9 = 7;
+let num10 = 2;
+let remainder = num9 % num10;
+console.log('Modulus Operator: 7 % 2 = ' + remainder);
+
